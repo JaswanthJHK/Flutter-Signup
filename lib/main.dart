7697565cb6ef5252cff8adde3b37aed9b4aee04e
JaswanthJHK <jaswanthkriz@gmail.com>
2023-04-03 21:19:22 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+      ),
+      home: HomeScreen(),
+    );
+    
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 142, 219, 255),
+      appBar: AppBar(),
+    );
+  }
+}
